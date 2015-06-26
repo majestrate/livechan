2e59b5db77e9dda0c71003ab9cf2f5c50c59a7ff
jeff <ampernand@gmail.com>
2015-06-26 11:17:28 -0400

    fix front page board list

--- static/welcome.js
+++ static/welcome.js
@@ -2,9 +2,9 @@
 // welcome page
 //
 
-var boardList = document.getElementById("boards");
 
 addBoardToList = function(boardName) {
+  var boardList = document.getElementById("boards");
   var elem = document.createElement("div");
   elem.className = "livechan_board_list_entry";
   var link = document.createElement("a");
@@ -14,14 +14,17 @@ addBoardToList = function(boardName) {
   boardList.appendChild(elem);
 }
 
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function() {
-  if ( ajax.status == 200 && ajax.readyState == XMLHttpRequest.DONE ) {
-    var boardList = JSON.parse(ajax.reponseText);
-    for ( var idx = 0 ; idx < boardList; idx ++ ) {
-      addBoardToList(boardList[idx]);
+window.addEventListener('load', function() {
+
+  var ajax = new XMLHttpRequest();
+  ajax.onreadystatechange = function() {
+    if ( ajax.status == 200 && ajax.readyState == XMLHttpRequest.DONE ) {
+      var boardList = JSON.parse(ajax.reponseText);
+      for ( var idx = 0 ; idx < boardList; idx ++ ) {
+        addBoardToList(boardList[idx]);
+      }
     }
   }
-}
-ajax.open("GET", "/channels");
-ajax.send();
+  ajax.open("GET", "/channels");
+  ajax.send();
+});
