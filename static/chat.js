a04a7c7146a67dab6e2cbae6a47a4c4f1e19f357
jeff <ampernand@gmail.com>
2015-12-21 15:43:51 -0500

    try fixing linkifier

--- static/chat.js
+++ static/chat.js
@@ -242,6 +242,7 @@ var messageRules = [
   [/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/g, function(m) {
     var out = document.createElement("a");
     out.href = m[1];
+    out.target = "_blank";
     out.textContent = m[1];
     return out;
   }],
